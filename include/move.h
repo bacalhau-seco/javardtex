e7b2c6e4fd171a99a485c73099d3aa062995b93e
bacalhau <bacalhau@based.pt>
2026-06-18 22:16:41 +0100

    FINALLY fixed movement for good

--- include/move.h
+++ include/move.h
@@ -1,14 +1,15 @@
 #import "raylib.h"
 
-#define GRAVITY         20.0f
-#define MAX_SPEED       20.0f
-#define JUMP_FORCE      6.5f
-#define FRICTION        0.85f
-#define STAND_HEIGHT    1.74f
-#define BOTTOM_HEIGHT   0.5f
-#define CROUCH_HEIGHT   0.6f
-#define ACCEL           90.0f
-#define AIR_ACCEL       90.0f 
+#define GRAVITY         800.0f
+#define MAX_SPEED       250.0f
+#define JUMP_FORCE      270.0f
+#define FRICTION          4.0f
+#define STOPSPEED       100.0f
+#define STAND_HEIGHT     56.0f
+#define BOTTOM_HEIGHT    32.0f
+#define CROUCH_HEIGHT    32.0f
+#define ACCEL            5.0f
+#define AIR_ACCEL        10.0f 
 
 // Body structure
 typedef struct {
