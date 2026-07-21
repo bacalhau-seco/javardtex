c51421b8c2d50f44f189722c7e5f452e19ebe918
bacalhau <bacalhau@based.pt>
2026-07-22 00:41:36 +0100

    test

--- src/cl_main.c
+++ src/cl_main.c
@@ -40,7 +40,7 @@ int main(void)
 
     DisableCursor();        // Limit cursor to relative movement inside the window
 
-    SetTargetFPS(60);       // Set our game to run at 60 frames-per-second
+    //SetTargetFPS(60);       // Set our game to run at 60 frames-per-second
     //--------------------------------------------------------------------------------------
 
     // Main game loop
